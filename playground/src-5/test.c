f4d055cd0ac999f9a24df3a9404f7c41dce4dea6
JHlotov <iglotov@tektelic.com>
2023-10-17 22:04:10 +0300

    UPDATE: BMP280 test

--- playground/src-5/test.c
+++ playground/src-5/test.c
@@ -11,7 +11,7 @@
 int main(int argc, char* argv[])
 {
     int fd;
-    unsigned char val;
+    unsigned char id;
 
     fd = open("/dev/i2c-1", O_RDWR);
     if (fd < 0)
@@ -22,9 +22,16 @@ int main(int argc, char* argv[])
 
     ioctl(fd, I2C_SLAVE, BMP280_ADDRESS);
 
-    val = i2c_smbus_read_word_data(fd, BMP280_REVISION_REG);
+    id = i2c_smbus_read_word_data(fd, BMP280_REVISION_REG);
     
-    printf("BMP280 ID: %#x\n", val);
+    if (id == 0x58)
+    {
+        printf("BMP280 successfully detected: ID = 0x%02hhx\n", id);
+    }
+    else
+    {
+        printf("BMP280 not detected\n");
+    }
 
     close (fd);
 
